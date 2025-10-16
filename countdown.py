29c683a3c4048d34a87660f16914a74a538399be
Daphne Cluseau <daphne.cluseau@fms-ea.com>
2025-10-16 14:33:09 +0200

    init main fic

---
+++ countdown.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import random
+
+# Le programme permet à l’utilisateur de jouer à ce jeu célèbre. Il tire au sort un nombre à obtenir entre 101 et 999, ainsi que 6 plaques – portant un nombre – tirés parmi 24 réparties ainsi : chaque nombre de 1 à 10 est présent 2 fois, et les nombres 25, 50, 75 et 100 sont présents en un seul exemplaire.
+# A chaque tour de jeu, l’utilisateur sélectionne une opération (addition, soustraction, multiplication, division) et 2 nombres parmi les plaques restantes et les nombres (restants) obtenus lors des opérations précédentes. L’opération appliquée à ces nombres (qui ne sont alors plus utilisables) fournit alors un nouveau nombre.
+# L’utilisateur peut, quand il estime ne pas pouvoir se rapprocher davantage du nombre à obtenir, indiquer qu’il souhaite arrêter le jeu (en indiquant si besoin quel nombre parmi ceux obtenus et restants est le nombre qu’il a obtenu), celui-ci s’arrêtant également lorsqu’il ne reste plus de plaque disponible et un seul nombre résultant d’opérations.
+# Le programme indique alors à l’utilisateur si « le compte est bon » (le nombre à obtenir a été atteint) ou s’il n’a obtenu qu’un nombre approchant le nombre à obtenir.
+
+# Constants
+# Number to calculate
+NB_TO_CALCULATE_MIN = 101
+NB_TO_CALCULATE_MAX = 999
+# Number of plates to draw
+NB_DRAWN_PLATES = 6
+# Plates
+PLATES_TO_DRAW = {"doubles" : [1,2,3,4,5,6,7,8,9,10], "simples" : [25, 50, 75, 100]}
+# Plates drawn
+plates_drawn = []
+
+# Generate random number to calculate in range 101, 999
+def generate_nb_to_calculate():
+    """
+
+    :return: number to calculate
+    """
+    nb_to_calculate = random.randint(NB_TO_CALCULATE_MIN,NB_TO_CALCULATE_MAX)
+    return nb_to_calculate
+
+# Draw 6 plates
+def draw_plates(arr_plates):
+    """
+
+    :param arr_plates: list of drawn plates
+    :return: list of drawn plates
+    """
+    # For each plate, generate random choice between simple or double
+    for i in range(NB_DRAWN_PLATES):
+        random_choice = random.choice(list(PLATES_TO_DRAW.keys()))
+        plate_drawn = random.randint(PLATES_TO_DRAW[random_choice][0],PLATES_TO_DRAW[random_choice][len(PLATES_TO_DRAW[random_choice]) - 1])
+        if random_choice == "simple":
+            while plate_drawn in arr_plates:
+                plate_drawn = random.randint(PLATES_TO_DRAW[random_choice][0],PLATES_TO_DRAW[random_choice][len(PLATES_TO_DRAW[random_choice]) - 1])
+        else:
+            while arr_plates.count(plate_drawn) > 1:
+                plate_drawn = random.randint(PLATES_TO_DRAW[random_choice][0],PLATES_TO_DRAW[random_choice][len(PLATES_TO_DRAW[random_choice]) - 1])
+        arr_plates.append(plate_drawn)
+    return arr_plates
